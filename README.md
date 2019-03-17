a9e5d42ff85c777995cea5f95f1fbf2c81b7dff8
Jan Hrček <honza.hrk@gmail.com>
2019-03-17 13:04:06 +0100

    Highlight central node when showing node context

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ A simple web app for traversing large directed graphs.
 ##TODO
 
 - [x] given FGL graph, render it using graphviz as svg image
-- [ ] Highlight the central node, when viewing node in its context
-- [ ] toggle to enable / disable transitive closure
+- [x] Highlight the central node, when viewing node in its context
+- [ ] toggle to enable / disable transitive reduction
--- app/Main.hs
+++ app/Main.hs
@@ -7,44 +7,61 @@ import Control.Monad.IO.Class (MonadIO, liftIO)
 import Data.Graph.Inductive.Graph (Node, gelem, neighbors, subgraph)
 import Data.GraphViz (GraphvizParams, defaultParams, fmtNode, globalAttributes,
                       graphToDot)
-import Data.GraphViz.Attributes
+import Data.GraphViz.Attributes (shape, fillColor, style, filled)
 import Data.GraphViz.Attributes.Complete
 import Data.GraphViz.Commands (GraphvizOutput (Svg), runGraphviz)
 import Data.GraphViz.Types.Generalised
 import Data.Text.Lazy (fromStrict, pack)
+import Data.GraphViz.Attributes.Colors.SVG (SVGColor(LightSalmon))
 import Elm.Analyse (loadModuleDependencies)
 import Graph (ClusterLabel, EdgeLabel, MyGraph, NodeLabel)
 import Web.Scotty
+import qualified Data.IntSet as ISet
 
 main :: IO ()
 main = do
   modDepsGraph <- loadModuleDependencies "a.json"
   scotty 3000 $ do
       get "/" $
-          generateGraphFile modDepsGraph >>= file
+          generateWholeGraph modDepsGraph >>= file
 
       get "/node/:nodeId" $ do
           nodeId :: Int <- param "nodeId"
           case getNeighborhood nodeId modDepsGraph of
               Nothing -> text $ "This graph doesn't have node with id " <> pack (show nodeId)
-              Just neighborhood -> generateGraphFile neighborhood >>= file
+              Just neighborhood -> generateGraphFileWithHighlightedNode nodeId neighborhood >>= file
 
 
-graphVizParams :: GraphvizParams Node NodeLabel EdgeLabel ClusterLabel NodeLabel
-graphVizParams = defaultParams
+type MyGraphvizParams = GraphvizParams Node NodeLabel EdgeLabel ClusterLabel NodeLabel
+
+graphVizParams :: [Node] -> MyGraphvizParams
+graphVizParams nodesToHighlight = defaultParams
     { globalAttributes = [ GraphAttrs [RankDir FromLeft] ]
     , fmtNode = \(nodeId, nodeLabel) ->
+        (if shouldHighlight nodeId then ([style filled, fillColor LightSalmon]++) else id)
         [ shape BoxShape
         , URL $ "/node/" <> pack (show nodeId)
         , Label . StrLabel $ fromStrict nodeLabel
         ]
     }
+    where
+      highlight = ISet.fromList nodesToHighlight
+      shouldHighlight nodeId = ISet.member nodeId highlight
 
-generateGraphFile :: MonadIO io => MyGraph -> io FilePath
-generateGraphFile g =
+generateWholeGraph :: MonadIO io => MyGraph -> io FilePath
+generateWholeGraph =
+    generateGraphFile (graphVizParams [])
+
+generateGraphFileWithHighlightedNode :: MonadIO io => Node ->  MyGraph -> io FilePath
+generateGraphFileWithHighlightedNode nodeId =
+    generateGraphFile (graphVizParams [nodeId])
+
+
+generateGraphFile :: MonadIO io => MyGraphvizParams -> MyGraph -> io FilePath
+generateGraphFile gvParams g =
      liftIO $ runGraphviz dotGraph Svg "graph.svg"
    where
-     dotGraph = graphToDot graphVizParams g
+     dotGraph = graphToDot gvParams g
 
 getNeighborhood :: Node -> MyGraph -> Maybe MyGraph
 getNeighborhood nodeId gr
