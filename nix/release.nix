2b00b56361ada975871d5568c179734b57033a09
Marek Fajkus <marek.faj@gmail.com>
2019-09-22 18:52:02 +0200

    Nix build improvements (#14)
    * Use uglify-js from nixpkgs
    
    * simplify further
    
    * simplify flags in readme instructions
    
    * improve runtime deps

--- nix/release.nix
+++ nix/release.nix
@@ -1,5 +1,4 @@
 let
-  nodePackages = import ./node-composition.nix {};
   client = import ./client.nix {};
 
   config = {
@@ -11,13 +10,16 @@ let
               (haskell.lib.overrideCabal
                 (haskellPkgsNew.callPackage ./server.nix {})
                 ( oldDerivation: {
-                  preConfigure = ''
-                    mkdir -p client/dist/js
-                    ${nodePackages.uglify-js}/bin/uglifyjs ${client}/Main.js \
-                      --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
-                      | ${nodePackages.uglify-js}/bin/uglifyjs --mangle --output=client/dist/js/elm.js
-                  '';
-                })
+                    preConfigure = ''
+                      mkdir -p client/dist/js
+                      uglifyjs ${client}/Main.js \
+                        --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+                        | uglifyjs --mangle --output=client/dist/js/elm.js
+                    '';
+                    executableHaskellDepends = [ pkgs.nodePackages_10_x.uglify-js ];
+                    executableSystemDepends = with pkgs; [ graphviz elmPackages.elm-analyse ];
+                  }
+                )
               );
           };
         };
@@ -26,15 +28,5 @@ let
 
   pkgs = import <nixpkgs> { inherit config; };
 
-  elmTools = import (pkgs.fetchFromGitHub {
-    owner = "turboMaCk";
-    repo = "nix-elm-tools";
-    rev = "41b5045587f84d993a7ee55972cfd61152cafc48";
-    sha256 = "1ns02xxj3zijf6myaxk8azgs8v69gpc2b0v080m2xjf1pvv6hd75";
-  }) { inherit pkgs; };
-
-in with pkgs; {
-  inherit graphviz;
-  inherit (haskellPackages) grbr;
-  inherit (elmTools) elm-analyse;
-}
+in
+  pkgs.haskellPackages.grbr
