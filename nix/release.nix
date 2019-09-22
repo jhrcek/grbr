ed62b2b9ab62d7c277003f1969172fca29119cd4
Marek Fajkus <marek.faj@gmail.com>
2019-09-22 14:23:34 +0200

    simplify further

--- nix/release.nix
+++ nix/release.nix
@@ -16,8 +16,7 @@ let
                         --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
                         | uglifyjs --mangle --output=client/dist/js/elm.js
                     '';
-                    executableHaskellDepends = oldDerivation.executableHaskellDepends
-                      ++ [ pkgs.nodePackages_10_x.uglify-js ];
+                    executableHaskellDepends = [ pkgs.nodePackages_10_x.uglify-js ];
                   }
                 )
               );
