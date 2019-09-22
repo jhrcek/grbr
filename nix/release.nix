87056d2acba41fd4ade7912f751dea5cbbbf1fa9
Marek Fajkus <marek.faj@gmail.com>
2019-09-22 14:20:43 +0200

    Use uglify-js from nixpkgs

--- nix/release.nix
+++ nix/release.nix
@@ -1,5 +1,4 @@
 let
-  nodePackages = import ./node-composition.nix {};
   client = import ./client.nix {};
 
   config = {
@@ -11,13 +10,16 @@ let
               (haskell.lib.overrideCabal
                 (haskellPkgsNew.callPackage ./server.nix {})
                 ( oldDerivation: {
-                  preConfigure = ''
-                    mkdir -p client/dist/js
-                    ${nodePackages.uglify-js}/bin/uglifyjs ${client}/Main.js \
-                      --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
-                      | ${nodePackages.uglify-js}/bin/uglifyjs --mangle --output=client/dist/js/elm.js
-                  '';
-                })
+                    preConfigure = ''
+                      mkdir -p client/dist/js
+                      uglifyjs ${client}/Main.js \
+                        --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' \
+                        | uglifyjs --mangle --output=client/dist/js/elm.js
+                    '';
+                    executableHaskellDepends = oldDerivation.executableHaskellDepends
+                      ++ [ pkgs.nodePackages_10_x.uglify-js ];
+                  }
+                )
               );
           };
         };
